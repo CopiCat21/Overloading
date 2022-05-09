a270b66f9bcb7c51f609a51fc822b197549d89e7
Benjamin Aldrin <94682503+CopiCat21@users.noreply.github.com>
2022-05-09 23:04:31 +0700

    Update main.cpp

--- Overloading/src/main.cpp
+++ Overloading/src/main.cpp
@@ -59,6 +59,7 @@ int main()
     int i;
     while (n)
     {
+        Clear();
         cout << "Pilih Operator yang digunakan : \n" << endl;
         cout << "1. Apakah persegi panjang berpotongan atau tidak" << endl;
         cout << "2. Menggabungkan luas persegi panjang" << endl;
@@ -66,6 +67,7 @@ int main()
         cout << "4. Memperluas persegi panjang" << endl;
         cout << "5. Memperkecil persegi panjang" << endl;
         cout << "6. Properti Kelas";
+        cout << "0. Keluar Program";
         cout << "\nPilih Menu : ";
         cin >> i;
         cin.ignore();
@@ -123,16 +125,19 @@ int main()
             break;}
 
         case 6:
-            {Clear();
-            cout << "xmin pertama   : " << pertama[1] << endl;
-            cout << "xmax pertama   : " << pertama[2] << endl;
-            cout << "ymin pertama   : " << pertama[3] << endl;
-            cout << "ymax pertama   : " << pertama[4] << endl;
-            cout << "xmin kedua     : " << kedua[1] << endl;
-            cout << "xmax kedua     : " << kedua[2] << endl;
-            cout << "ymin kedua     : " << kedua[3] << endl;
-            cout << "ymax kedua     : " << kedua[4] << endl;
-            break;}
+            {
+            Clear();
+            cout << "xMin pertama   : " << pertama[1] << endl;
+            cout << "xMax pertama   : " << pertama[2] << endl;
+            cout << "yMin pertama   : " << pertama[3] << endl;
+            cout << "yMax pertama   : " << pertama[4] << endl;
+            cout << "xMin kedua     : " << kedua[1] << endl;
+            cout << "xMax kedua     : " << kedua[2] << endl;
+            cout << "yMin kedua     : " << kedua[3] << endl;
+            cout << "yMax kedua     : " << kedua[4] << endl;
+            Enter();
+            break;
+            }
         
         case 0:
             {return 0;}
